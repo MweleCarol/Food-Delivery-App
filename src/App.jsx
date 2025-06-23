6427a96083e5451407186221b7db11ea551b56b5
William550-debug <williamnjoroge2019@gmail.com>
2025-06-23 20:41:01 +0300

    Few Changes to the interactiveness

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "./components/Navbar/Navbar";
+import Navbar from "./Components/Navbar/Navbar";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Cart from "./pages/Cart/Cart";
