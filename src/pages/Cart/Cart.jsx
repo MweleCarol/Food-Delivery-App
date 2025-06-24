8dd1aa985a2043c82066f1097a44c5df5f295a10
MweleCarol <mwelecarolyne65@gmail.com>
2025-06-24 13:12:39 +0300

    Added the cart page and the place order page

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,11 +1,13 @@
 import React, { useContext } from 'react';
 import './Cart.css'
 import {StoreContext} from '../../Context/StoreContext'
+import { useNavigate } from 'react-router-dom';
 
 
 const Cart = () => {
 
-    const{cartItems,food_list,removeFromCart} = useContext(StoreContext);
+  const{cartItems,food_list,removeFromCart, getTotalCartAmount} = useContext(StoreContext);
+  const navigate = useNavigate();
 
   return (
     <div className='cart'>
@@ -28,9 +30,9 @@ const Cart = () => {
                   <div className='cart-items-title cart-items-item'>
                   <img src={item.image} alt="" />
                   <p>{item.name}</p>
-                  <p>{item.price}</p>
+                  <p>${item.price}</p>
                   <p>{cartItems[item._id]}</p>
-                  <p>{item.price * cartItems[item._id]}</p>
+                  <p>${item.price * cartItems[item._id]}</p>
                   <p onClick={()=>removeFromCart(item._id)} className='cross'>X</p>
                 </div>
                 <hr />
@@ -39,7 +41,37 @@ const Cart = () => {
             }
         })}
       </div>
-      
+      <div className="cart-bottom">
+        <div className="cart-total">
+          <h2>Cart Totals</h2>
+          <div>
+            <div className="cart-total-details">
+              <p>Subtotal</p>
+              <p>${getTotalCartAmount()}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <p>Delivery Fee</p>
+              <p>${getTotalCartAmount() === 0 ? 0 : 2}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <b>Total</b>
+              <b>${getTotalCartAmount() === 0 ? 0 : getTotalCartAmount()+ 2}</b>
+            </div>
+          </div>
+           <button onClick={() =>navigate('/order')}>PROCEED TO CHECKOUT</button>
+        </div>
+        <div className="cart-promocode">
+          <div>
+            <p>If you have a promo code, enter it here</p>
+            <div className="cart-promocode-input">
+              <input type="text" placeholder='promocode' />
+              <button>Submit</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
